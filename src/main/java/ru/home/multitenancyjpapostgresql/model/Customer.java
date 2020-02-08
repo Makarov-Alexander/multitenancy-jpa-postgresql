c3214752e53a2003dafc85cede27684022388203
Makarov Alexander <mak.alex88@gmail.com>
2020-02-08 09:30:15 +0700

    add Customer model

---
+++ src/main/java/ru/home/multitenancyjpapostgresql/model/Customer.java
@@ -0,0 +1,32 @@
+package ru.home.multitenancyjpapostgresql.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
+
