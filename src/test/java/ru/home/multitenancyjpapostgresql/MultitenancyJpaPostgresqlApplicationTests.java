46a433008289a43257ce0058ef56f9359642d3fb
Makarov Alexander <mak.alex88@gmail.com>
2020-02-08 08:31:58 +0700

    init repository

---
+++ src/test/java/ru/home/multitenancyjpapostgresql/MultitenancyJpaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.multitenancyjpapostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenancyJpaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
