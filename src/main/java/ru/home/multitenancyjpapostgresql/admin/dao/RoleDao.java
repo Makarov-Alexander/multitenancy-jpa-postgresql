e975b6e18aba2d7346fd2e5650b2af40cce7f5d7
Makarov Alexander <mak.alex88@gmail.com>
2020-03-15 11:28:21 +0700

    set uniName for em

--- src/main/java/ru/home/multitenancyjpapostgresql/admin/dao/RoleDao.java
+++ src/main/java/ru/home/multitenancyjpapostgresql/admin/dao/RoleDao.java
@@ -8,33 +8,38 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class RoleDao {
 
-//    @PersistenceContext
+    @PersistenceContext(unitName = "admin")
     private EntityManager entityManager;
 
     public RoleDao(
-//            @Qualifier("adminEntityManagerFactory") EntityManagerFactory adminEntityManagerFactory
+            @Qualifier("adminEntityManagerFactory") EntityManagerFactory adminEntityManagerFactory
     ) {
-//        this.entityManager = adminEntityManagerFactory.createEntityManager();
+        this.entityManager = adminEntityManagerFactory.createEntityManager();
+        Map<String, Object> properties = adminEntityManagerFactory.getProperties();
+        properties.forEach((s, o) -> System.out.printf("+++ %s: %s\n", s, o.toString()));
     }
 
     public void setRole(String role) {
-//        Query nativeQuery = entityManager.createNativeQuery("SET ROLE " + role);
-//        nativeQuery.executeUpdate();
+        Query nativeQuery = entityManager.createNativeQuery("SET ROLE " + role);
+        nativeQuery.executeUpdate();
     }
 
     public void createRole(String role) {
-//        Query nativeQuery = entityManager.createNativeQuery(
-//                String.format(
-//                    "RESET ROLE;\n" +
-//                    "CREATE ROLE %1$s;\n" +
-//                    "GRANT %1$s TO customer_none;\n" +
-//                    "CREATE TABLE customer_%1$s PARTITION OF customer FOR VALUES IN (\'%1$s\');", role));
-//        nativeQuery.executeUpdate();
+        System.out.println("+++ CURRENT_USER: " +
+                entityManager.createNativeQuery("select current_user").getSingleResult().toString());
+
+        Query nativeQuery = entityManager.createNativeQuery(
+                String.format(
+                    "CREATE ROLE %1$s;\n" +
+                    "GRANT %1$s TO customer_none;\n" +
+                    "CREATE TABLE customer_%1$s PARTITION OF customer FOR VALUES IN (\'%1$s\');", role));
+        nativeQuery.executeUpdate();
     }
 
 }
