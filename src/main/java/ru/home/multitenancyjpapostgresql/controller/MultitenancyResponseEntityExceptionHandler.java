b30cc2444da54e8341659a6721486d1743d5a2f0
Makarov Alexander <al.makarov@cft.ru>
2020-03-25 18:03:57 +0700

    add controller error handler

---
+++ src/main/java/ru/home/multitenancyjpapostgresql/controller/MultitenancyResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package ru.home.multitenancyjpapostgresql.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class MultitenancyResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = { RuntimeException.class })
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
